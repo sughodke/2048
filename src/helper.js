05e5374c235e1d7be0d87de85a6fcce8415ca5c7
Sid <sid.ghodke@gmail.com>
2021-09-02 01:26:29 -0700

    Sort out movement logic

--- src/helper.js
+++ src/helper.js
@@ -44,10 +44,10 @@ export function moveBoard(direction, gameState) {
   /* Assume we are moving downwards (v),
    *  here are the 3 cases we care about.
    *
-   *  A
-   *  v B
-   *  v v
-   *  X C D
+   *  A        |
+   *  0 B   E  v
+   *  0 0   F
+   *  0 C D 0
    *
    */
   function* generateNextHit({x, y, xOffset, yOffset}) {
@@ -55,7 +55,7 @@ export function moveBoard(direction, gameState) {
     do {
       destX += xOffset
       destY += yOffset
-      destValue = newBoard[destX][destY]
+      let destValue = newBoard[destX][destY]
 
       yield { destX, destY, destValue}
     } while (destX >= 0 && destX < size && destY >= 0 && destY < size)
@@ -68,7 +68,6 @@ export function moveBoard(direction, gameState) {
 
       newBoard[x][y] = 0
 
-
       let [destX, destY] = [x, y]
       let destValue
       try {
@@ -76,24 +75,17 @@ export function moveBoard(direction, gameState) {
           destX += xOffset
           destY += yOffset
           destValue = newBoard[destX][destY]
-        } while (destValue !== 0)
+        } while (destValue === 0)
+
+        if (destValue === srcValue) {
+          newBoard[destX][destY] = srcValue << 1
+        } else {
+          // oops went too far
+          throw new Error('go back one')
+        }
       } catch (e) {
-        // oops went too far, go back one
+        // got to the end of the board
         newBoard[destX-xOffset][destY-yOffset] = srcValue
-
-        // got to the end of the board, move to the next column
-        continue
-      }
-
-      newBoard[x][y] = 0
-      // if (srcValue === 0) {
-        // do nothing
-      // } else if (destValue === 0) {
-      // } else
-      if (destValue === srcValue) {
-        newBoard[destX][destY] = srcValue << 1
-      } else {
-        newBoard[x][y] = srcValue
       }
     }
   }
